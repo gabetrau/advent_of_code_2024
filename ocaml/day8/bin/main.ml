cadbfd0373df39fadd38a9d0cfdb2c171c29d5ea
gabe <gabetrau@gmail.com>
2024-12-12 17:04:32 -0600

    save

--- ocaml/day8/bin/main.ml
+++ ocaml/day8/bin/main.ml
@@ -9,7 +9,31 @@ let rec read_in istrm lines =
     if line = "" then lines else read_in istrm (line :: lines)
   with End_of_file -> lines
 
+let mark_antinodes grid tenna1 tenna2 =
+  let difx = fst tenna2 - fst tenna1 in
+  let dify = snd tenna2 - snd tenna1 in
+  let ant1 = (fst tenna1 - difx, snd tenna1 - dify) in
+  let ant2 = (fst tenna2 + difx, snd tenna2 + dify) in
+  if fst ant1 >= 0 && snd ant1 >= 0 && fst ant1 < Array.length grid && snd ant1 < Array.length grid.(0) then
+    let tenna1_point = grid.(fst tenna1).(snd tenna1) in
+    
+    let ant1_point = grid.(fst ant1).(snd ant1) in
+    let ant2_point = grid.(fst ant2).(snd ant2) in
+    if ant1_point = '.' then
+      grid.(fst ant1).(snd ant1) <- { antenna = tenna1_point.antenna ; antinode = true };
+    else 
+      grid.(fst ant1).(snd ant1) <- { antenna = ant1_point.antenna ; antinode = true };
+    if grid.(fst ant2).(snd ant2) = '.' then
+      grid.(fst ant2).(snd ant2) <- { antenna = tenna1_point.antenna ; antinode = true };
+    else
+      grid.(fst ant2).(snd ant2) <- { antenna = ant2_point.antenna ; antinode = true };
+    grid
+
 let solve_part_1 grid total =
+  let rec count_antinodes grid =
+    Array.iter (fun r ->
+      Array.iter (fun c -> if c.)
+
 
 
 let () =
